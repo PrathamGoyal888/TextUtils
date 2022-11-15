9f021d651eefcbda790f0c34d3875be5ebd8dab4
PrathamGoyal888 <prathamgoyal9425@gmail.com>
2022-11-15 17:59:02 +0530

    Updates

---
+++ precache-manifest.cdb75e793a36a2e83c436277a9213da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e655ac5b4a601047961e",
+    "url": "/TextUtils/static/css/main.e60798dc.chunk.css"
+  },
+  {
+    "revision": "e655ac5b4a601047961e",
+    "url": "/TextUtils/static/js/main.b31ef880.chunk.js"
+  },
+  {
+    "revision": "48102987d2a039063e70",
+    "url": "/TextUtils/static/js/runtime~main.b1d3a562.js"
+  },
+  {
+    "revision": "0a19cd6d519594f8e7a5",
+    "url": "/TextUtils/static/js/2.0f44089b.chunk.js"
+  },
+  {
+    "revision": "eac2663a493c35f8869d",
+    "url": "/TextUtils/static/js/3.a2d6af6c.chunk.js"
+  },
+  {
+    "revision": "1c3e2c29418986037391dc4a2987794f",
+    "url": "/TextUtils/index.html"
+  }
+];
